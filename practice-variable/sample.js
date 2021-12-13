624f7805f508ca81b16da2d0951277a042bed5e1
RouxRhett <nsch.4871@gmail.com>
2021-12-13 22:11:40 +0900

    変数の使い分けについての学習

---
+++ practice-variable/sample.js
@@ -0,0 +1,27 @@
+// varによる再代入
+//var nickname = "taro"
+//console.log(nickname)
+//nickname = "jiro"
+//console.log(nickname)
+
+// letによる再宣言
+//let nickname = "taro"
+//console.log(nickname)
+//let nickname = "ichiro"
+//console.log(nickname)
+
+// constによる再宣言
+//const nickname = "taro"
+//console.log(nickname)
+//const nickname = "ichiro"
+//console.log(nickname)
+
+var str = "webcamp"
+
+function foo() { 
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
